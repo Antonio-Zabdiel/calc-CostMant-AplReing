78bc91e7d33961224d6fd7bb2fc250beaad6f642
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2023-02-24 10:53:37 -0600

    completo

--- src/App.js
+++ src/App.js
@@ -1,69 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h2>
-          Calculadora del costo de beneficio de realizar una reingenieria
-        </h2>
-      </div>
+      <div className="div-top"><h2>
+        Calculadora del costo de beneficio de realizar una reingenieria
+      </h2></div>
 
-      <div>
+      <div className="div-form">
         <forms id="datos">
           <label>Costo actual de mantenimiento: </label>
-          <input type='number' id='P1'></input>
+          <input type='number' id='P1' className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
 
           <label>Costo actual de operacion: </label>
-          <input type='number' id='P2'></input>
+          <input type='number' id='P2' className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
 
           <label>Valor empresarial de la aplicacion (Anual): </label>
-          <input type='number' id='P3'></input>
+          <input type='number' id='P3'  className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
 
           <label>Costo de mantenimiento despues de la reingenieria: </label>
-          <input type='number' id='P4'></input>
+          <input type='number' id='P4'  className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
 
           <label>Costo operacional despues de la reingenieria: </label>
-          <input type='number' id='P5'></input>
+          <input type='number' id='P5'  className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
           
           <label>Valor empresarial de la aplicacion despues de la reingenieria: </label>
-          <input type='number' id='P6'></input>
+          <input type='number' id='P6'  className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
 
           <label>Costo de reingenieria: </label>
-          <input type='number' id='P7'></input>
+          <input type='number' id='P7'  className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
 
           <label>Tiempo de la reingenieria: </label>
-          <input type='number' id='P8'></input>
+          <input type='number' id='P8'  className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
 
           <label>Costo actual de mantenimiento: </label>
-          <input type='number' id='L'></input>
+          <input type='number' id='L'  className="inputnum"></input>
 
-          <br/>
+          <br/><br/>
 
-          <input type='button' onClick={Calcular} value={"Calcular"}></input>
+          <input type='button' className="boton" onClick={Calcular} value={"Calcular"}></input>
         </forms>
       </div>
 
       <br/>
-
-      <div id='res'></div>
+        
+      <div className="div-cent"><p id='res' className="div-res"></p></div>
     </div>
   );
 }
@@ -79,25 +76,30 @@ function Calcular(){
   var p8 = parseInt(document.getElementById("P8").value);
   var l = parseInt(document.getElementById("L").value);
 
-
   var mantenimientoActual = costMant(p1,p2,p3,l);
   var costoReingenieria = costReing(p4, p5, p6, p7, p8, l);
-  var beneficioEcon = beneficio(p1,p2,p3,p4,p5,p6,p7,p8,l)
-  alert(beneficioEcon)
-
-  //aqui ira la cadena final
-  //document.getElementById("res").innerHTML = costoReingenieria;
+  var beneficioEcon = beneficio(p1,p2,p3,p4,p5,p6,p7,p8,l);
+  var valeing = valeRein(p1,p2,p3,p4,p5,p6,p7,p8,l);
   
+  var texto = "<div><p>mantenimiento actual: " + mantenimientoActual + "</p>" +
+              "<p>costo de la reingenieria: " + costoReingenieria + "</p>" +
+              "<p>beneficio economico: " + beneficioEcon + "</p>" + 
+              "<p>" + valeing +"</p></div>" ;
+  
+  document.getElementById("res").innerHTML = texto;
 }
+
 
 function costMant(p1, p2, p3, l){
   var cos_mante = (p3-(p1+p2))*l;
+
   return cos_mante;
 }
 
 function costReing(p4, p5, p6, p7, p8, l){
   var p9 = 1;
   var cos_reing = (p6-((p4+p5)*(l-p8))-(p7*p9));
+
   return cos_reing;
 }
 
@@ -106,7 +108,19 @@ function beneficio(p1,p2,p3,p4,p5,p6,p7,p8,l){
   var reing = costReing(p4, p5, p6, p7, p8, l);
 
   var benefic = reing - mantAct;
+
   return benefic;
+}
+
+function valeRein(p1,p2,p3,p4,p5,p6,p7,p8,l){
+  var beneficioEcon = beneficio(p1,p2,p3,p4,p5,p6,p7,p8,l);
+
+  if(beneficioEcon > 0){
+    return "vale la pena hacer reingenieria :3";
+  }
+  else{
+    return "no vale la pena hacer reingenieria :(";
+  }
 }
 
 export default App;
